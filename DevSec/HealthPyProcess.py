7b8637cd4b1c17a93dc3fea94e1f18e8f7d44a02
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2023-02-12 19:43:56 +0100

    checking open file of first process

--- DevSec/HealthPyProcess.py
+++ DevSec/HealthPyProcess.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import signal
 from threading import Thread
 import time
 from tkinter import ttk
@@ -6,48 +7,63 @@ import tkinter as tk
 import psutil
 #from PIL import ImageTk,Image
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime
 
 ## APP CONFIG
 root = tk.Tk()
 label = tk.Label(root, text="placeholder")
 # Window
-root.title("HealthPyProcess");
+root.title("HealthPy Process🌱");
 root.minsize(400, 600)
 # Frame
 frm = ttk.Frame(root, padding=100)
 frm.grid()
 
 label = tk.Label(frm, text="placeholder")
-
+def handler(signum, frame):
+    root.destroy
+    exit()
 def stats():
-    info = "CPU: " + str(psutil.cpu_percent()) + "%\n" \
-    + "MEM: " + str(psutil.virtual_memory().percent) + "%"
+
+    info = "[ CPU and RAM usage ]\n\n" \
+    + "CPU: " + str(psutil.cpu_percent()) + "%\n" \
+    + "MEM: " + str(psutil.virtual_memory().percent) + "%\n"
     label['text'] = info
     label.grid(column=0, row=0)
     root.after(750, stats)
 
-
+def scan(processes):
+    print(processes[0].open_files())
 def app():
     
     processes = [psutil.Process(pid=p.info['pid']) for p in psutil.process_iter(['pid', 'name'])]
-    # for p in psutil.process_iter(['pid', 'name']):
-    #     print(psutil.Process(pid=p.info['pid']))
+
 
     # Sort the processes by memory usage
     processes = sorted(processes, key=lambda p: p.memory_info().rss, reverse=True)
 
     # Print the memory usage and process name
-    result = ""
+    result = "  [ Most Abusing Process ]\n\n"
     for i in range(5):
-        result += "Process name:"+ str(processes[i].name()) + "\n"
-        result += "Memory usage:"+ str(processes[i].memory_info().rss / 1024 / 1024) + "MB" + "\n\n"
+        result += "Process name: "+ str(processes[i].name())  + "\n"
+        result += "Memory usage: "+ str(round(processes[i].memory_info().rss / 1024 / 1024,2)) + " MB" + "\n\n"
+        
     ttk.Label(frm, text=result).grid(column=0, row=1)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=2)
+    ttk.Button(frm, text="Scan",command=lambda: scan(processes)).grid(column=0, row=2)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=3)
+    
+    root.after(2000, app)
+    
 
 if __name__== '__main__':
-    stats()
-    app()
+    signal.signal(signal.SIGINT, handler)
+    try:
+        stats()
+        app()
+        #scan()
+    except:
+        print("An exception occurred")
+        root.destroy
+        exit()
     root.mainloop()
 
